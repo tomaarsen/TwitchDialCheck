ecc3b526e3deefa81a9f3b75c0d2170dba49111b
CubieDev <cubiegamedev@gmail.com>
2019-03-05 10:52:27 +0100

    Update DialCheck.py

--- DialCheck.py
+++ DialCheck.py
@@ -32,6 +32,7 @@ class Settings:
 
 class DialCheck:
     def __init__(self):
+        # Variables with None are overridden with the Settings class
         self.host = None
         self.port = None
         self.chan = None
@@ -43,8 +44,9 @@ class DialCheck:
         Settings(self)
 
         self.track = 0
-        self.tracked = []#[True, False, True, False, False]
+        self.tracked = []
 
+        # Start the websocket.
         self.ws = TwitchWebsocket(self.host, self.port, self.message_handler, live=False)
         self.ws.login(self.nick, self.auth)
         self.ws.join_channel(self.chan)
@@ -59,28 +61,32 @@ class DialCheck:
         self.t = t
 
     def isAccepted(self, message):
-        return "D I A L" in message or "dangDial" in message or "DIAL" in message
+        # Check if message contains a DIAL message of any kind
+        return "D I A L" in message or "dangDial" in message or "dial" in message.lower()
 
     def message_handler(self, m):
         if m.type == "PRIVMSG":
-            if m.message.startswith("!power"):
-                self.ws.send_message("The office and studio lost power for the past 24 hours, Dan is setting up the show now. Please enjoy the Tark Squad play, normal shows will resume on Wednesday with the new game, new studio! dangC")
-
+            # Check if message contains a DIAL message of any kind
+            # And update self.track aka time when previous DIAL message occurred 
             if self.isAccepted(m.message):
-                #print("Dial recorded")
                 self.track = time.time()
             
+            # If it's within self.t seconds since the previous DIAL, add True or False to self.tracked
+            # Based on if the message is a DIAL message
             if self.track > time.time() - self.t:
                 self.tracked.append(self.isAccepted(m.message))
                 print(self.tracked)
             
+            # If it's been over self.t seconds since the previous DIAL
             elif len(self.tracked):
+                # Remove trailing Falses
                 for b in reversed(self.tracked):
                     if b:
                         break
                     else:
                         self.tracked.pop(-1)
 
+                # Get amount of true values
                 trues = sum([b for b in self.tracked if b])
                 
                 # Get ratio of messages that is DIAL related
@@ -91,32 +97,29 @@ class DialCheck:
                 
                 # Get result as a function from 10 to 100
                 result = min(percentage * ratio * 10 * 0.9 + 10 + random.uniform(-1, 1), 100)
-               
+                
                 print(f"Amt of Trues\t: {trues:}\nRatio\t\t: {ratio * 100:.2f}%\nPercentage\t: {percentage:.2f}%")
+                # Output dial percentage to chat
                 self.ws.send_message(f"Chat is {result:.2f}% dialed.")
+
+                # Reset stored Trues and Falses
                 self.tracked = []
     
     def current_viewers(self):
+        # Returns amount of current viewers for channel
         i = 0
         while i < 20:
             try:
+                # Attempt to get chatter count
                 data = requests.get(f"https://tmi.twitch.tv/group/user/{self.chan.replace('#', '')}/chatters").json()
             except Exception as e:
                 print(e)
                 i += 1
             else:
-                # Return + 1 to prevent division by 0
                 print(data["chatter_count"], "chatters")
+                # Return amount of chatters divided by 1.5
+                # This value seems to provide a decent result
                 return data["chatter_count"] / 1.5 if data["chatter_count"] != 0 else 200
 
 if __name__ == "__main__":
     DialCheck()
-
-'''
-[True, True, True, True, False, True, True, True, True, True, True, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, True, False, True, True, False]
-500 chatters
-Amt of Trues    : 30
-Ratio           : 88.24%
-Percentage      : 6.00%
-Chat is 57.65% dialed.
-'''
